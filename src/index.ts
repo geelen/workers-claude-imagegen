00c7c1df7fc01f0bb08b2f28b1d6e283359c86b9
Glen Maddern <glen@cloudflare.com>
2024-12-13 15:43:12 +1100

    demo complete

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { WorkerEntrypoint } from 'cloudflare:workers';
+import { ProxyToSelf } from 'workers-mcp';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+export default class ClaudeImagegen extends WorkerEntrypoint<Env> {
+	/**
+	 * Generate an image using the `flux-1-schnell` model. Works best with 8 steps.
+	 *
+	 * @param {string} prompt - A text description of the image you want to generate.
+	 * @param {number} steps - The number of diffusion steps; higher values can improve quality but take longer. Must be between 4 and 8, inclusive.
+	 * */
+	async generateImage(prompt: string, steps: number) {
+		const response = await this.env.AI.run('@cf/black-forest-labs/flux-1-schnell', {
+			prompt,
+			steps,
+		})
+		// Convert from base64 string
+		const binaryString = atob(response.image)
+		// Create byte representation
+		const img = Uint8Array.from(binaryString, (m) => m.codePointAt(0))
+		return new Response(img, {
+			headers: {
+				'Content-Type': 'image/jpeg',
+			},
+		})
+	}
+
+	async fetch(request: Request): Promise<Response> {
+		return new ProxyToSelf(this).fetch(request)
+	}
+}
